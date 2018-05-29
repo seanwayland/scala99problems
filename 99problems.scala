7ae8ea247512640e763b4090e6ecea9983338f17
seanwayland <seanwayland@gmail.com>
2018-05-29 15:56:12 -0400

    Update 99problems.scala

--- 99problems.scala
+++ 99problems.scala
@@ -199,4 +199,15 @@ def duplicate[A](ls: List[A]): List[A] = {
 }
 
 
+/***
+P15 (**) Duplicate the elements of a list a given number of times.
+Example:
+  scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+  */
 
+def duplicateN[A](n: Int,ls: List[A]): List[A] = {
+  ls flatMap  { e => List.fill(n)(e)  }
+}
+
+   
