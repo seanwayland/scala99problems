b3c8f78d183f8cacb716ad2616cc9a1b1cba54ce
seanwayland <seanwayland@gmail.com>
2018-05-29 15:35:48 -0400

    Update 99problems.scala

--- 99problems.scala
+++ 99problems.scala
@@ -146,8 +146,44 @@ object P09 {
 }
 */
    
+ /***  
+  (*) Modified run-length encoding.
+Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N, E) terms.
+Example:
+
+scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+*/
+     def encodeModified[A](ls: List[A]): List[Any] =
+    encode(ls) map { t => if (t._1 == 1) t._2 else t }
    
    
+   /*** 
+   P12 (**) Decode a run-length encoded list.
+Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+Example:
+
+scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
+*/
+   def decode[A](ls: List[(Int, A)]): List[A] =
+  ls flatMap { e => List.fill(e._1) (e._2) }
+   
+   // P13 (**) Run-length encoding of a list (direct solution).
+//     Implement the so-called run-length encoding data compression method
+//     directly.  I.e. don't use other methods you've written (like P09's
+//     pack); do all the work directly.
+//
+//     Example:
+//     scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+//     res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+   def encodeDirect[A](ls: List[A]): List[(Int, A)] =
+    if (ls.isEmpty) Nil
+    else {
+      val (packed, next) = ls span { _ == ls.head }
+      (packed.length, packed.head) :: encodeDirect(next)
+    }
    
    
  
